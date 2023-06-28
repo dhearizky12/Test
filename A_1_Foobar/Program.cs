11fa23b4a46f90332dadb15d843a8a8dd3f38e3f
dhearizky12 <dherizdhea@gmail.com>
2023-06-28 14:27:10 +0700

    initial commit

---
+++ A_1_Foobar/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number (n): ");
+        int n;
+        try
+        {
+            n = Convert.ToInt32(Console.ReadLine());
+            FoobarPrinter foobarPrinter = new FoobarPrinter(n);
+            foobarPrinter.PrintNumbers();
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+    }
+}
+
+class FoobarPrinter
+{
+    private int n;
+
+    public FoobarPrinter(int number)
+    {
+        n = number;
+    }
+
+    public void PrintNumbers()
+    {
+        for (int x = 1; x <= n; x++)
+        {
+            if (x % 3 == 0 && x % 5 == 0)
+                Console.Write("foobar ");
+            else if (x % 3 == 0)
+                Console.Write("foo ");
+            else if (x % 5 == 0)
+                Console.Write("bar ");
+            else
+                Console.Write(x + " ");
+        }
+        Console.WriteLine(); // New line after printing all numbers
+    }
+}
